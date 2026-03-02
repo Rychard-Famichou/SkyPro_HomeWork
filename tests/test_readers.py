9b9b50e0fa5152051544b804be053d83973f3c5b
Rychard <rychard.famichou@gmail.com>
2026-03-02 11:30:32 +0100

    Linters and README

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -1,13 +1,19 @@
-import pytest
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
 import pandas as pd
-from unittest.mock import patch, MagicMock
+import pytest
+
 from src import readers
 
 
-@pytest.mark.parametrize("reader_func, pd_method, file_path", [
-    (readers.read_data_csv, "pandas.read_csv", "test.csv"),
-    (readers.read_data_excel, "pandas.read_excel", "test.xlsx")
-])
+@pytest.mark.parametrize(
+    "reader_func, pd_method, file_path",
+    [
+        (readers.read_data_csv, "pandas.read_csv", "test.csv"),
+        (readers.read_data_excel, "pandas.read_excel", "test.xlsx"),
+    ],
+)
 def test_readers_success(reader_func, pd_method, file_path):
     """Универсальный тест успешного чтения для CSV и Excel"""
     # Добавить json_load из utils?
@@ -27,23 +33,26 @@ def test_readers_success(reader_func, pd_method, file_path):
         # Проверяем
         assert result == expected_data
         mock_pd.assert_called_once_with(file_path)
-        mock_df.to_dict.assert_called_once_with(orient='records')
+        mock_df.to_dict.assert_called_once_with(orient="records")
 
 
-@pytest.mark.parametrize("reader_func, pd_method", [
-    (readers.read_data_csv, "pandas.read_csv"),
-    (readers.read_data_excel, "pandas.read_excel")
-])
-@pytest.mark.parametrize("exception", [
-    FileNotFoundError,
-    pd.errors.EmptyDataError,
-    pd.errors.ParserError,
-    TypeError,
-    ValueError,
-    UnicodeDecodeError('utf-8', b'', 1, 2, 'reason') # требует аргументы для инициализации
-])
+@pytest.mark.parametrize(
+    "reader_func, pd_method",
+    [(readers.read_data_csv, "pandas.read_csv"), (readers.read_data_excel, "pandas.read_excel")],
+)
+@pytest.mark.parametrize(
+    "exception",
+    [
+        FileNotFoundError,
+        pd.errors.EmptyDataError,
+        pd.errors.ParserError,
+        TypeError,
+        ValueError,
+        UnicodeDecodeError("utf-8", b"", 1, 2, "reason"),  # требует аргументы для инициализации
+    ],
+)
 def test_all_readers_errors(pd_method, reader_func, exception):
-    """ Общий тест для CSV и Excel на обработку ошибок """
+    """Общий тест для CSV и Excel на обработку ошибок"""
     # Добавить json_load из utils?
 
     # Используем patch как контекстный менеджер, так как путь к методу меняется
